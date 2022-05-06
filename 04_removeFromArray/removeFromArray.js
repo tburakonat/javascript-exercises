433cb343de359e42cf8a96e90e2caa8e83afeda6
tburakonat <tburakonat@gmail.com>
2022-05-06 23:39:05 +0200

    completed excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...params) {
+    const array = params[0]
+    return array.filter(item => !params.includes(item))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
