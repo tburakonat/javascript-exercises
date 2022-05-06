d1f4220843c0aef54fc59b36ab9a0b283526e075
tburakonat <tburakonat@gmail.com>
2022-05-06 22:11:57 +0200

    complete excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const stringArray = string.split("")
+    const reversedArray = []
+    for (let i = 0; stringArray.length; i++) {
+        const letter = stringArray.shift()
+        reversedArray.unshift(letter)
+    }
+    return reversedArray.join("")
+}
 
 // Do not edit below this line
 module.exports = reverseString;
