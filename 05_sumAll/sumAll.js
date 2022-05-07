29ed9f4a877670cc1028530df472174d5e0679ec
tburakonat <tburakonat@gmail.com>
2022-05-07 12:02:05 +0200

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    if (start < 0 || end < 0 || typeof(start) !== "number"  || typeof(end) !== "number" ) {
+        return "ERROR"
+    } else if (start > end) {
+        [start, end] = [end, start]
+        console.log("Start", start)
+        console.log("End", end)
+    } 
+    for (let i = start; i <= end; i++) {
+       sum = sum + i 
+    }
+    return sum
 };
 
 // Do not edit below this line
