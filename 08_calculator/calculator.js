86297cb625083252e167b83fbc10ad87d6449a95
tburakonat <tburakonat@gmail.com>
2022-05-15 13:01:22 +0200

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y 
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0
+  arr.forEach(element => {
+    sum += element
+  });
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length) {
+    return arr.reduce((previousValue, currentValue) => previousValue * currentValue)
+  } else {
+    return 0
+  }
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1
+  } else {
+    let product = 1
+    for (let i = 1; i <= num; i++) {
+      product *= i
+    }
+    return product
+  }
 };
 
 // Do not edit below this line
