2880184cba8a093bb0a4312c663160cd51dfd342
tburakonat <tburakonat@gmail.com>
2022-05-15 14:24:40 +0200

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sortedArr = arr
+                        .sort((a, b) => {
+                            if (!a.yearOfDeath) {
+                                a.yearOfDeath = (new Date).getFullYear()
+                            }
+                            if (!b.yearOfDeath) {
+                                b.yearOfDeath = (new Date).getFullYear()
+                            }
+                            return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1
+                        })
+    return sortedArr[0]
 };
 
 // Do not edit below this line
