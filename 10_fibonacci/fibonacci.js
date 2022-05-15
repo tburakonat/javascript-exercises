9bb871d6cdbdb9ae4b6ec90df9b5e5f922647228
tburakonat <tburakonat@gmail.com>
2022-05-15 13:53:50 +0200

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let num = parseInt(input)
+    if (num < 1) return `OOPS`
+    let oldFibo = 0
+    let newFibo = 1
+    let temp
+    for (let i = 0; i < num; i++) {
+        temp = newFibo
+        newFibo = oldFibo + newFibo
+        oldFibo = temp
+    }
+    return oldFibo
 };
 
 // Do not edit below this line
